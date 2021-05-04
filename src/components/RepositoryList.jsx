4873d52eb9a9dd4a98366216e5dd4dfcd117cf14
dalissongabriel <alissongabrieldino@gmail.com>
2021-05-04 19:29:55 -0300

    chore: first commit start project

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,25 @@
+import { RepositoryItem } from "./RepositoryItem";
+import { useEffect, useState } from "react";
+
+import "../styles/repositories.scss";
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de respositórios</h1>
+      <ul>
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.id} repository={repository} />
+        ))}
+      </ul>
+    </section>
+  );
+}
